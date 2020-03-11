d5de236d40cbf5ab256cb17b55c6cb979edecb14
Cesar <cesarvasz@gmail.com>
2020-03-10 21:57:27 -0600

    User controller created

--- controllers/users.js
+++ controllers/users.js
@@ -13,6 +13,50 @@ const createUser = (req, res) => {
     })
 }
 
+const loginUser = (req, res) => {
+    var userId = req.body.userId
+
+    Users.findById(userId, (err, user) => { 
+        if(err) { res.status(404).send({ err }) }
+
+        res.status(200).send({ message: "Logged successfully", user })
+       })
+}
+
+const getUsers = (req, res) => {
+    Users.find((err, users) => {
+        if(err) { res.status(404).send({ err }) }
+
+        res.status(200).send({ message: "Users retreived!", users})
+    })
+}
+
+const updateUser = function(req, res){
+    var userId = req.params.userId
+    var toUpdate = req.body
+
+    Users.findByIdAndUpdate(userId, toUpdate, (err, userUpdated) => {
+        if(err) { res.status(404).send({ err }) }
+
+        res.status(200).send({ message: "User updated", userUpdated})
+    })
+}
+
+const deleteUser = function(req, res){
+    var userId = req.params.userId
+
+    Users.findByIdAndRemove(userId, (err, userDeleted) => {
+        if(err) { res.status(404).send({ err }) }
+
+        res.status(200).send({ message: "User deleted", userDeleted})
+    })
+}
+
+
 module.exports = {
-    createUser
+    createUser,
+    loginUser,
+    getUsers,
+    updateUser,
+    deleteUser
 }
--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,9 @@ const router = express.Router()
 const usersController = require("./../controllers/users.js")
 
 router.post("/createUser", usersController.createUser)
+router.post("/login", usersController.loginUser)
+router.get("/getUsers", usersController.getUsers)
+router.put("/updateUser/:userId", usersController.updateUser)
+router.delete("/deleteUser/:userId", usersController.deleteUser)
 
 module.exports = router
